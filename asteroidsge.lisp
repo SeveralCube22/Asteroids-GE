49488f1eba3198bd65bff39ff70fbcd8bcb2d928
Viswadeep Manam <manam.viswadeep@gmail.com>
2021-08-19 17:20:39 -0700

    Fixed rotation in render ship. Added controls to rotate ship.

--- asteroidsge.lisp
+++ asteroidsge.lisp
@@ -9,6 +9,14 @@
 (defparameter *screen-width* 640)
 (defparameter *screen-height* 480)
 (defparameter *ticks* 0)
+(defparameter *ship-wing-angle* 20)
+
+(defun get-ticks ()
+    (let* ((current (sdl:sdl-get-ticks))
+           (delta (- current *ticks*)))
+           
+        (setf *ticks* current)
+        (/ delta 1000.0)))
 
 (defclass ship ()
     ((angle  :accessor angle :initform 90.0 :initarg :angle)
@@ -21,7 +29,7 @@
      
      (ship-length :accessor ship-length :initform 25 :initarg :ship-length)
      
-     (rot-speed :accessor rot-speed :initform 3 :initarg :rot-speed)
+     (rot-speed :accessor rot-speed :initform 250 :initarg :rot-speed)
      
      (turning :accessor turning :initform nil :initarg :turning)))
 
@@ -30,30 +38,24 @@
 
 (defun point-from-angle-length (coord angle len)
     (sdl:point :x (+ (* (* len (cos (* angle (/ pi 180)))) -1) (sdl:x coord))
-           :y (+ (* (* len (sin (* angle (/ pi 180)))) -1) (sdl:y coord))))
+           :y (+ (* (* len (sin (* angle (/ pi 180)))) 1) (sdl:y coord))))
 
 (defun get-angle-in-range (angle)
     (cond ((> angle 360) (get-angle-in-range (- angle 360)))
           ((< angle 0) (get-angle-in-range (+ angle 360)))
           (t angle)))
-
-
+          
 (defmethod render ((ship ship))
         (let* ((nose (point-from-angle-length (pos ship) (angle ship) (ship-length ship)))
-              (func (cond ((> (angle ship) 90) #'+)
-                          ((< (angle ship) 90) #'-)
-                          (t (lambda (x y) x))))
-              (left (point-from-angle-length nose 
-                            (get-angle-in-range (funcall func 250 (angle ship))) (ship-length ship)))
-              (right (point-from-angle-length nose 
-                            (get-angle-in-range (funcall func 290 (angle ship))) (ship-length ship))))
+              (left (point-from-angle-length nose (+ (angle ship) *ship-wing-angle*) (ship-length ship)))
+              (right (point-from-angle-length nose (- (angle ship) *ship-wing-angle*) (ship-length ship))))
       
             (sdl:draw-line nose left :color sdl:*white*)
             (sdl:draw-line nose right :color sdl:*white*)
             (sdl:draw-line left right :color sdl:*white*)))
 
-(defmethod update ((ship ship))
-  (cond ((turning ship) (setf (angle ship) (+ (rot-speed ship) (angle ship))))))
+(defmethod update ((ship ship) delta-time)
+  (cond ((turning ship) (setf (angle ship) (get-angle-in-range (+ (angle ship) (* (rot-speed ship) delta-time)))))))
 
 (defun main ()
     (sdl:with-init  ()
@@ -66,14 +68,25 @@
 
             (:key-down-event (:key key)
               (case key
-                  (:sdl-key-a (setf (turning (ship world)) t))))
+                  (:sdl-key-a 
+                    (setf (turning (ship world)) t)
+                    (setf (rot-speed (ship world)) (* (rot-speed (ship world)) -1)))
+                  (:sdl-key-d 
+                    (setf (turning (ship world)) t)
+                    (setf (rot-speed (ship world)) (abs (rot-speed (ship world)))))  
+                    ))
 
             (:key-up-event (:key key)
               (case key
-                  (:sdl-key-a (setf (turning (ship world)) nil))))
+                  (:sdl-key-a 
+                    (setf (turning (ship world)) nil))
+                  
+                  (:sdl-key-d
+                    (setf (turning (ship world)) nil))))
+                  
             (:idle ()
                 (sdl:clear-display sdl:*black*)
-                (update (ship world))
+                (update (ship world) (get-ticks))
                 (render (ship world))
                 (sdl:update-display))))))
 
