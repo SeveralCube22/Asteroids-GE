d878c4762f4fac5c75495968187da0f836320681
Viswadeep Manam <manam.viswadeep@gmail.com>
2021-09-04 00:34:49 -0700

    Added asteroids movement and collision

--- asteroidsge.lisp
+++ asteroidsge.lisp
@@ -11,9 +11,14 @@
 (defparameter *ship-wing-angle* 15)
 (defparameter *ship-nose-len* -15)
 (defparameter *ship-wing-len* 25)
+(defparameter *ship-radius* 10)
 (defparameter *ship-max-velocity* 500.0) ; weird values because don't know how to setup delta-time
 (defparameter *ship-acceleration* 100)
 (defparameter *ship-acceleration-scaling* .90)
+(defparameter *asteroid-max-states* 3)
+(defparameter *max-asteroids-spawn* 5)
+(defparameter *asteroid-init-speed* 70)
+(defparameter *asteroid-init-radius* 30)
 
 (defun get-ticks ()
     (let* ((current (sdl:sdl-get-ticks))
@@ -24,23 +29,25 @@
 
 (defclass ship ()
     ((angle  :accessor angle :initform 90.0 :initarg :angle)
-     
      (velocity-angle :accessor velocity-angle :initform 0.0 :initarg ::velocity-angle)
-
      (pos :accessor pos :initform (sdl:point :x 320 :y 240) :initarg :pos)
-     
      (acceleration :accessor acceleration :initform 0.0 :initarg :acceleration)
-    
      (velocity :accessor velocity :initform 0.0 :initarg :velocity)
-     
      (rot-speed :accessor rot-speed :initform 300 :initarg :rot-speed)
-     
      (turning :accessor turning :initform nil :initarg :turning)
-     
      (thrusting :accessor thrusting :initform nil :initarg :thrusting)))
 
+(defclass asteroid ()
+    ((velocity-angle :accessor velocity-angle :initform (+ 0 (random 340)) :initarg :velocity-angle)
+     (velocity :accessor velocity :initarg :velocity)
+     (pos :accessor pos :initform (sdl:point :x (random 640) :y (random 480)) :initarg :pos)
+     (state :accessor state :initarg :state) 
+     (radius :accessor radius :initarg :radius)))
+
 (defclass world ()
-  ((ship :accessor ship :initform (make-instance 'ship) :initarg :ship)))
+  ((ship :accessor ship :initform (make-instance 'ship) :initarg :ship)
+   (asteroids :accessor asteroids :initform nil :initarg :asteroids)
+   (game-over :accessor game-over :initform nil :initarg :game-over))) ; list of asteroids
 
 (defun point-from-angle-length (coord angle len)
     (sdl:point :x (+ (sdl:x coord) (* (* len (cos (* angle (/ pi 180)))) -1))
@@ -60,6 +67,9 @@
             (sdl:draw-line nose right :color sdl:*white*)
             (sdl:draw-line left right :color sdl:*white*)))
 
+(defmethod render ((asteroid asteroid))
+  (sdl:draw-circle (pos asteroid) (radius asteroid) :color sdl:*white*))
+
 
 (defmethod thrust ((ship ship))
     (setf (acceleration ship) (+ (* *ship-acceleration-scaling* (acceleration ship)) 1))
@@ -77,9 +87,7 @@
                   ((and (< v2x 0) (>= v2y 0)) (setf velocity-angle (+ velocity-angle 180)))
                   ((and (< v2x 0) (< v2y 0))  (setf velocity-angle (+ velocity-angle 180))))
 
-            (setf (velocity-angle ship) velocity-angle)
-            
-            ))
+            (setf (velocity-angle ship) velocity-angle)))
 
 (defmethod stop-thrust ((ship ship))
     (setf (acceleration ship) 0.0))
@@ -104,24 +112,56 @@
          (x (+ (sdl:x (pos ship)) dx))
          (y (- (sdl:y (pos ship)) dy)))
         
+        (cond ((< x 0) (setf x 640))
+               ((> x 640) (setf x 0)))
+
+        (cond ((< y 0) (setf y 480))
+               ((> y 480) (setf y 0)))
+
+        (setf (pos ship) (sdl:point :x x :y y)))
+
+  (render ship))
+
+(defmethod update ((asteroid asteroid) delta-time)
+   (let* ((vx (* (velocity asteroid) (cos (* (velocity-angle asteroid) (/ pi 180))))) 
+          (vy (* (velocity asteroid) (sin (* (velocity-angle asteroid) (/ pi 180)))))
+          (dx (* vx delta-time))
+          (dy (* vy delta-time))
+          (x (+ (sdl:x (pos asteroid)) dx))
+          (y (- (sdl:y (pos asteroid)) dy)))
+        
          (cond ((< x 0) (setf x 640))
                ((> x 640) (setf x 0)))
 
-         (cond ((< y 0) (setf y 480))
+        (cond ((< y 0) (setf y 480))
                ((> y 480) (setf y 0)))
 
-         (setf (pos ship) (sdl:point :x x :y y))
-         )
-  
-  ;(check-collision)
-  )
+        (setf (pos asteroid) (sdl:point :x x :y y)))
+        
+  (render asteroid))
+
+(defmethod update ((world world) delta-time)
+  (update (ship world) delta-time)
+  (dolist (asteroid (asteroids world))
+           (update asteroid delta-time)
+           (cond ((> (expt (+ *ship-radius* (radius asteroid)) 2) (+ (expt (- (sdl:x (pos (ship world))) (sdl:x (pos asteroid))) 2)
+                                                                     (expt (- (sdl:y (pos (ship world))) (sdl:y (pos asteroid))) 2))) 
+                      (setf (game-over world) t)))))
+
+(defmethod init-asteroids ((world world))
+   (dotimes (number *max-asteroids-spawn*)
+      (setf (asteroids world) (cons (make-instance 'asteroid :state 1 :velocity *asteroid-init-speed* :radius *asteroid-init-radius*) 
+                                    (asteroids world))))
+      
+    (asteroids world))
 
 (defun main ()
     (sdl:with-init  ()
         (sdl:window *screen-width* *screen-height* :title-caption "Asteroids")
         (setf (sdl:frame-rate) 60)
         
-      (let ((world (make-instance 'world)))
+      (let* ((world (make-instance 'world)))
+       (init-asteroids world)
        (sdl:with-events ()
             (:quit-event () t)
 
@@ -153,8 +193,8 @@
                   
             (:idle ()
                 (sdl:clear-display sdl:*black*)
-                (update (ship world) (get-ticks))
-                (render (ship world))
+                (if (not (game-over world)) (update world (get-ticks)))
                 (sdl:update-display))))))
 
 (main)
+
